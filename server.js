e708d35106078328df1c4bb6fb14d4f20a66ca5d
Gautham Krishna P <gauthamkrishna1312@gmail.com>
2024-06-09 12:58:40 +0530

    Server modified

--- server.js
+++ server.js
@@ -2,13 +2,41 @@ const http = require('node:http');
 
 const hostname = '127.0.0.1';
 const port = 3000;
+const fs = require('fs');
+const url = require('node:url');
 
 const server = http.createServer((req, res) => {
-  //res.statusCode = 200;
-  //res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
+
+  let q = url.parse(req.url, true);
+
+  if (req.url==='/') {
+    fs.readFile('home.html', (err, data) => {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/html');
+      res.write(data);
+      res.end();
+    });
+    
+  } else if (req.url==='/login') {
+    fs.readFile('login.html', (err, data) => {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/html');
+      res.write(data);
+      res.end();
+    });
+  }else {
+    fs.readFile('404.html', (err, data) => {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/html');
+      res.write(data);
+      res.end();
+      console.log(q.query);
+    });
+  }
 });
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+console 
